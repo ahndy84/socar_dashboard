21745c148b086375a44035836c46eead0315293b
salt <salt@socar.kr>
2018-11-15 20:16:43 +0900

    docker 환경세팅

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
 module.exports = {
-	server_port: 3000,
-	db_url: 'mongodb://localhost:27017/local',
+	//server_port: 3000,
+	server_port: process.env.PORT || 3030,
+	//db_url: 'mongodb://localhost:27017/local',
+	db_url: `mongodb://${process.env.DB_HOST || 'localhost'}:27017/${process.env.DB || 'local'}`,
+
 	db_schemas: [
 	    {file:'./user_schema', collection:'users5', schemaName:'UserSchema', modelName:'UserModel'}
 	],
